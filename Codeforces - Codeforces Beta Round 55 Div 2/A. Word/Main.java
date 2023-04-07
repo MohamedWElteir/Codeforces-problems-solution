1c893a6aaa769b55c93a0f96be342780af9351ee
Mohamed Wael <hfhybujj@gmail.com>
2023-04-07 16:30:20 +0200

    New problem solved

---
+++ Codeforces - Codeforces Beta Round 55 Div 2/A. Word/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        int uppercaseCount = 0, lowercaseCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                uppercaseCount++;
+            } else {
+                lowercaseCount++;
+            }
+        }
+        if (uppercaseCount > lowercaseCount) {
+            System.out.println(word.toUpperCase());
+        } else if (uppercaseCount < lowercaseCount) {
+            System.out.println(word.toLowerCase());
+        } else {
+            System.out.println(word.toLowerCase());
+        }
+    }
+}
